3728db33dd2de02ab73ddbb6014e71c753251dce
twonine <1185366392qq.com>
2025-10-19 17:36:29 +0800

    初始化 image，mixer，ttf

---
+++ build/CMakeFiles/SpaceFighter-Darwin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceFighter-Darwin.dir/main.cpp.o"
+  "CMakeFiles/SpaceFighter-Darwin.dir/main.cpp.o.d"
+  "SpaceFighter-Darwin"
+  "SpaceFighter-Darwin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceFighter-Darwin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
-int main(int argc, char *argv[]) {
+int main(int, char **) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
@@ -10,6 +13,34 @@ int main(int argc, char *argv[]) {
     SDL_Window *window = SDL_CreateWindow("Hello SDL!", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
     // 创建渲染器, -1表示使用第一个支持的渲染驱动，SDL_RENDERER_ACCELERATED表示使用硬件加速的渲染
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    //初始化 SDL_Image 初始化
+    if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) != (IMG_INIT_JPG | IMG_INIT_PNG)) {
+        std::cerr << "IMG_Init() Error: " << IMG_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Texture *texture = IMG_LoadTexture(renderer, "assets/image/bg.png");
+    //初始化 Mixer
+    if (Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        std::cerr << "Mix_OpenAudio() Error: " << Mix_GetError() << std::endl;
+        return 1;
+    }
+
+    // 读取音乐
+    Mix_Music *music = Mix_LoadMUS("assets/music/03_Racing_Through_Asteroids_Loop.ogg");
+    // 播放音乐
+    Mix_PlayMusic(music, -1);
+
+    //初始化 TTF
+    if (TTF_Init() != 0) {
+        std::cerr << "TTF_Init() Error: " << TTF_GetError() << std::endl;
+    }
+    //加载字体
+    TTF_Font *font = TTF_OpenFont("assets/font/VonwaonBitmap-16px.ttf", 24);
+    // 创建文本纹理
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface *surface = TTF_RenderUTF8_Solid(font, "Hello,SDL 第一个 Demo", color);
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, surface);
 
     bool isRunning = true;
     while (isRunning) {
@@ -21,13 +52,38 @@ int main(int argc, char *argv[]) {
         }
         //渲染前先清屏
         SDL_RenderClear(renderer);
+        //画长方形
         SDL_Rect rect = {100, 200, 100, 200};
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        //画图片
+        SDL_Rect dstrect = {200, 200, 200, 200};
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+        // 画字体
+        SDL_Rect textRect = {300, 300, surface->w, surface->h};
+        SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         //更新屏幕
         SDL_RenderPresent(renderer);
     }
+    //清理图片资源
+    SDL_DestroyTexture(texture);
+    IMG_Quit();
+
+    //清理音乐资源
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    Mix_Quit();
+
+    //清理字体资源
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(textTexture);
+    TTF_CloseFont(font);
+    TTF_Quit();
+
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
