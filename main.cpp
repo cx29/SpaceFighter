86dfa7f8c40f2ee283cf28f04b0498ef17ded0ce
twonine <1185366392qq.com>
2025-10-19 09:12:26 +0800

    初始化项目，在窗口中画一个红色的矩形

--- main.cpp
+++ main.cpp
@@ -2,6 +2,35 @@
 #include <SDL.h>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    // 创建窗口
+    SDL_Window *window = SDL_CreateWindow("Hello SDL!", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+    // 创建渲染器, -1表示使用第一个支持的渲染驱动，SDL_RENDERER_ACCELERATED表示使用硬件加速的渲染
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool isRunning = true;
+    while (isRunning) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false;
+            }
+        }
+        //渲染前先清屏
+        SDL_RenderClear(renderer);
+        SDL_Rect rect = {100, 200, 100, 200};
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        //更新屏幕
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
